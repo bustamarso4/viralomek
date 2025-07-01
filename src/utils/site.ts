4fbfb0afcf1a0fc99e4353815c5fcc2dcf27b969
bustamarso4 <bustamarso4@gmail.com>
2025-07-01 12:43:33 +0700

    Add files via upload

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,4 +1,4 @@
 export const nama = 'Viral Omek';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Viral Sma Korea Tante China';
+export const desk = 'Video Bokep Indo Jepang Omek Jav Hijab Live Tante Viral Terbaru Bocil Ngentot Jilbab Smp Skandal';
 export const url = 'https://viralomek.pages.dev';
 export const terbit = '2025-06-27T00:01:03Z';
